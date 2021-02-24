af8ee4f838d58d69f54e2f5c65eefa1b565a1bd2
Joanna Jaworska <joannajaw76@gmail.com>
2021-02-24 15:26:55 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const firstName = 'Joanna';
+const age = 24;
+
+
+alert ('Powiedz przyjacielu i wejdź!');
+console.log (firstName);
+console.log (age);
+
+console.log (`Nazywam się ${firstName} i mam ${age} lata`)
