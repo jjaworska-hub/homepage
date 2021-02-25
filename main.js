33e973cea79b8a309167af42d657d50f131315f5
Joanna Jaworska <joannajaw76@gmail.com>
2021-02-25 18:20:28 +0100

    center the page content

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-const firstName = 'Joanna';
+const firstName = "Joanna";
 const age = 24;
 
+alert("Powiedz przyjacielu i wejdź!");
+console.log(firstName);
+console.log(age);
 
-alert ('Powiedz przyjacielu i wejdź!');
-console.log (firstName);
-console.log (age);
-
-console.log (`Nazywam się ${firstName} i mam ${age} lata`)
+console.log(`Nazywam się ${firstName} i mam ${age} lata`);
