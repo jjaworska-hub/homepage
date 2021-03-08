5fb206f31d1b67b12326022b539062e9efb22d00
Joanna Jaworska <joannajaw76@gmail.com>
2021-03-08 15:37:12 +0100

    simplifying css, centering the body and navigation,adding notes,adding flex

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 const firstName = "Joanna";
 const age = 24;
 
+
+const greet = (age,firstName) =>{
+    console.log(
+        `Witaj Drogi Odwiedzający, nazywam się ${firstName} i mam ${age} lata.`
+    );
+}
+
+greet(25,'Wojtek');
+
+
 console.log(firstName);
 console.log(age);
 
 console.log(`Nazywam się ${firstName} i mam ${age} lata`);
 
-const notes = document.querySelector('.notes-JavaScript--js')
+const notes = document.querySelector('.notes-JavaScript--js');
 
-notes.innerHTML = 'Funkcje'
+notes.innerHTML = 'JavaScript';
+
